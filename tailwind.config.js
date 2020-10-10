53577dc421b0bfcfd038aa39f84fdff2b14a85a0
pomplad <inamkb@yahoo.com>
2020-10-10 23:57:29 +0500

    done header and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   purge: [`_site/**/*.html`],
   theme: {
-    extend: {},
+    fontFamily: { myfont: ["Open Sans", "sans-serif"] },
+    extend: {
+      colors: {
+        dark1: "#2d2d2d",
+        dark2: "#171717",
+      },
+      inset: {
+        half: "50%",
+      },
+    },
   },
   variants: {},
   plugins: [],
